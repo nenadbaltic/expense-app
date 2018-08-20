9111c15e910ff21acc5f0a93fb97e9c2f1b7f51c
Nenad <baltic.nenad@gmail.com>
2018-08-20 22:06:17 +0200

    format updated

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -65,7 +65,7 @@ export default class ExpenseForm extends React.Component {
             this.setState(() => { return { error: '' } })
             this.props.onSubmit({
                 description: this.state.description,
-                amount: parseFloat(this.state.amount, 10) * 10,
+                amount: parseFloat(this.state.amount, 10) * 100,
                 createdAt: this.state.createdAt.valueOf(),
                 note: this.state.note
             })
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -9,7 +9,7 @@ const ExpenseListItem = (props) => {
         <div>
             <Link to={`/edit/${props.id}`}><h3>{props.description}</h3></Link>
             <p>
-            {numeral(props.amount / 10).format('$0,0.00')}
+            {numeral(props.amount / 100).format('$0,0.00')}
              - 
              {moment(props.createdAt).format('MMMM Do, YYYY')}</p>
         </div>
