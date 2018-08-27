a28f0775e36690467e007f3b4997f1320d458abe
Nenad <baltic.nenad@gmail.com>
2018-08-27 18:18:58 +0200

    new2

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -40,3 +40,28 @@ export const editExpense = (id, updates) => {
         updates: updates
     }
 };
+
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => {
+    return {
+        type: 'SET_EXPENSES',
+        expenses: expenses
+    }
+}
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                })
+            })
+
+            dispatch(setExpenses(expenses));
+         });    
+    };
+};
